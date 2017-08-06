a0b874908c5c20ec4e88776771a27dbb4afd976d
Dead_off <AnterKan1@gmail.com>
2017-08-06 20:23:57 +0300

    refactoring: changed class name

--- src/main/java/maxim/z/FSUtils.java
+++ src/main/java/maxim/z/FSUtils.java
@@ -2,7 +2,7 @@ package maxim.z;
 
 import java.nio.ByteBuffer;
 
-public class FileSystemHelper {
+public class FSUtils {
 
     public static byte[] intAsFourBytes(int value) {
         return ByteBuffer.allocate(4).putInt(value).array();
--- src/test/java/maxim/z/FSUtilsTest.java
+++ src/test/java/maxim/z/FSUtilsTest.java
@@ -4,35 +4,35 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 
-public class FileSystemHelperTest {
+public class FSUtilsTest {
 
     @Test
     public void intAsFourBytes() {
-        byte[] actual = FileSystemHelper.intAsFourBytes(0);
+        byte[] actual = FSUtils.intAsFourBytes(0);
         byte[] expected = new byte[]{(byte) 0, (byte) 0, (byte) 0, (byte) 0};
         assertArrayEquals(expected, actual);
 
-        actual = FileSystemHelper.intAsFourBytes(1);
+        actual = FSUtils.intAsFourBytes(1);
         expected = new byte[]{(byte) 0, (byte) 0, (byte) 0, (byte) 1};
         assertArrayEquals(expected, actual);
 
-        actual = FileSystemHelper.intAsFourBytes(257);
+        actual = FSUtils.intAsFourBytes(257);
         expected = new byte[]{(byte) 0, (byte) 0, (byte) 1, (byte) 1};
         assertArrayEquals(expected, actual);
 
-        actual = FileSystemHelper.intAsFourBytes(655617);
+        actual = FSUtils.intAsFourBytes(655617);
         expected = new byte[]{(byte) 0, (byte) 0x0A, (byte) 1, (byte) 1};
         assertArrayEquals(expected, actual);
 
-        actual = FileSystemHelper.intAsFourBytes(-655361);
+        actual = FSUtils.intAsFourBytes(-655361);
         expected = new byte[]{(byte) 0xFF, (byte) 0xF5, (byte) 0xFF, (byte) 0xFF};
         assertArrayEquals(expected, actual);
 
-        actual = FileSystemHelper.intAsFourBytes(Integer.MAX_VALUE);
+        actual = FSUtils.intAsFourBytes(Integer.MAX_VALUE);
         expected = new byte[]{(byte) 0x7F, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
         assertArrayEquals(expected, actual);
 
-        actual = FileSystemHelper.intAsFourBytes(Integer.MIN_VALUE);
+        actual = FSUtils.intAsFourBytes(Integer.MIN_VALUE);
         expected = new byte[]{(byte) 0x80, (byte) 0, (byte) 0, (byte) 0};
         assertArrayEquals(expected, actual);
     }

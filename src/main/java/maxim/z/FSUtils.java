331d2da4d1f04d4561f8265081529b7995d8b822
Dead_off <AnterKan1@gmail.com>
2017-08-22 13:47:12 +0300

    replaced public method to package private

--- src/main/java/maxim/z/FSUtils.java
+++ src/main/java/maxim/z/FSUtils.java
@@ -3,24 +3,24 @@ package maxim.z;
 import java.nio.ByteBuffer;
 import java.util.regex.Pattern;
 
-public class FSUtils {
+class FSUtils {
 
     private final static Pattern NAME_PATTERN = Pattern.compile(String.format("^[A-Za-z0-9-_]{1,%s}$", FSConstants.FILE_NAME_LENGTH));
 
-    public static byte[] intAsFourBytes(int value) {
+    static byte[] intAsFourBytes(int value) {
         return ByteBuffer.allocate(4).putInt(value).array();
     }
 
-    public static int intFromFourBytes(byte[] bytes) {
+    static int intFromFourBytes(byte[] bytes) {
         return ByteBuffer.wrap(bytes).getInt();
     }
 
-    public static void writeIntAsBytesToArray(byte[] destArr, int destPosition, int value) {
+    static void writeIntAsBytesToArray(byte[] destArr, int destPosition, int value) {
         byte[] bytes = intAsFourBytes(value);
         System.arraycopy(bytes, 0, destArr, destPosition, bytes.length);
     }
 
-    public static String getNameWithSpaces(String name) {
+    static String getNameWithSpaces(String name) {
         StringBuilder nameWithSpaces = new StringBuilder(name);
         while (nameWithSpaces.length() < FSConstants.FILE_NAME_LENGTH) {
             nameWithSpaces.append(" ");
@@ -28,7 +28,7 @@ public class FSUtils {
         return nameWithSpaces.toString();
     }
 
-    public static boolean isCorrectName(String name) {
+    static boolean isCorrectName(String name) {
         return NAME_PATTERN.matcher(name).matches();
     }
 
